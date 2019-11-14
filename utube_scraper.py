a324f33d4d27303ecf11e9a35c09fe8553d861ef
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-11-14 19:51:03 +0100

    refactor to make more modular

--- utube_scraper.py
+++ utube_scraper.py
@@ -8,31 +8,14 @@ import googleapiclient.errors
 import json
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+def get_service():
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+def get_channel_id():
     channels = []
-    uploads = []
-    videos = []
-
-    with open("config.yaml", 'r') as ymlfile:
-        cfg = yaml.safe_load(ymlfile)
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    #client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
     # Ask for channel name ,test
     req_chnl = input("Which YouTube Channel do you want?\n")
-
-
-    #flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-       # client_secrets_file, scopes)
-    #credentials = flow.run_console()
-   # youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=cfg['api_key'])
 
     # Get ID based on channel name
     search_response = youtube.search().list(
@@ -46,22 +29,28 @@ def main():
         if search_result['id']['kind'] == 'youtube#channel':
            channels.append('%s (%s)' % (search_result['snippet']['title'],
                                      search_result['id']['channelId']))
-           fnd_id=search_result['id']['channelId']
-    print(search_response)
+           return search_result['id']['channelId']
     print("\nChannels Found:\n", '\n'.join(channels), '\n')
 
+    return None
+
+def get_uploads_list(fnd_id):
+    uploads = []
     # Get Uploads ID
     uploads_response = youtube.channels().list(
         part="contentDetails",
         id=fnd_id
     ).execute()
-    print("Uploads Response")
-    print(uploads_response)
 
     for uploads_result in uploads_response.get('items', []):
         #if uploads_result['kind'] == 'youtube#channel':
          #  uploads.append('%s' % (uploads_result['contentDetails']['relatedPlaylists']['uploads']))
-        uploads_id = uploads_result['contentDetails']['relatedPlaylists']['uploads']
+        return uploads_result['contentDetails']['relatedPlaylists']['uploads']
+
+    return None
+
+def get_video_list(uploads_id):
+    videos = []
 
     # Get video upload list
     video_response = youtube.playlistItems().list(
@@ -77,11 +66,37 @@ def main():
              #                          playlist_result['resourceId']['videoId']))
 
     print("Videos:\n", '\n'.join(videos), '\n')
+    return None
 
+def output_text(vid_txt):
     # List titles in file
     with open('video.txt', 'w') as file:
         # file.write(json.dumps(videos))
-        json.dump(videos, file, indent=2)
+        json.dump(vid_txt, file, indent=2)
+
+    return None
 
 if __name__ == "__main__":
-    main()
+    video_list = []
+    # main()
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    with open("config.yaml", 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    # client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    # client_secrets_file, scopes)
+    # credentials = flow.run_console()
+    # youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=cfg['api_key'])
+    channel_id = get_channel_id()
+    uploads_playlist_id = get_uploads_list(channel_id)
+    videos_list = get_video_list(uploads_playlist_id)
+    output_text(videos_list)
+    # print("An HTTP error  occurred")
